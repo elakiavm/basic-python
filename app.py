40e9e741efad50e089066f1efd493848a3480cdd
elakia <vmelakia@gmail.com>
2022-02-24 12:04:11 +0530

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+# print("hello world")
+# this is print statement 
+
+x = 10 
+y = 5
+list = ['cat','dog','rat']
+dict ={
+    "name":"cat",
+    "animals": "lion"
+}
+
+# if (y>x):
+#     print(" x is greater")
+
+# print(dict)
+
+a ="1000" 
+# print(a)
+# print(type(a))
+
+# for i in "elakia":
+#     print(i)
+
+i = 0
+
+while i<5:
+    print(i)
+    i = i + 1
